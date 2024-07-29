d967861ff332315d2067ad7309d44e9b3c4cc3f1
Alejandro <128639865+Ale241302@users.noreply.github.com>
2024-07-29 07:09:45 -0500

    Update Edit Users

--- database/migrations/2024_07_25_193337_create_sector_comercial_table.php
+++ database/migrations/2024_07_25_193337_create_sector_comercial_table.php
@@ -4,24 +4,41 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateSectorComercialTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('sector_comercial', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
             $table->timestamps();
         });
+
+        // Insertar datos iniciales
+        DB::table('sector_comercial')->insert([
+            ['nombre' => 'Retail (Venta al por menor)'],
+            ['nombre' => 'Comercio Mayorista'],
+            ['nombre' => 'Comercio Electrónico'],
+            ['nombre' => 'Alimentación y Bebidas'],
+            ['nombre' => 'Moda y Textiles'],
+            ['nombre' => 'Franquicias'],
+            ['nombre' => 'Servicios Comerciales'],
+            ['nombre' => 'Comercio Internacional'],
+        ]);
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('sector_comercial');
     }
-};
+}
